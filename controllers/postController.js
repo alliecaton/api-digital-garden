a68fff8eb57faf344e17d36792c817886ff54081
alliecaton <alliecaton3@gmail.com>
2023-11-12 18:50:35 -0800

    Added put & delete post endpoints

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,8 +1,10 @@
 const models = require('../models')
 
+const Post = models.Post
+
 exports.getPosts = async (req, res) => {
   try {
-    const data = await models.Post.findAndCountAll({
+    const data = await Post.findAndCountAll({
       order: [['updatedAt', 'DESC']],
     })
 
@@ -18,7 +20,7 @@ exports.getPost = async (req, res) => {
   const { id } = req.params
 
   try {
-    const data = await models.Post.findByPk(id)
+    const data = await Post.findByPk(id)
 
     if (data) {
       res.send(data.dataValues)
@@ -32,10 +34,51 @@ exports.createPost = async (req, res) => {
   const { title, content } = req.body
 
   try {
-    const data = await models.Post.create(
+    const data = await Post.create(
       { title: title, content: content },
-      { isNewRecord: true }
+      { isNewRecord: true },
+      {
+        returning: true,
+      }
     )
+
+    if (data) {
+      res.json(data)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.updatePost = async (req, res) => {
+  console.log(req.params)
+  const { title, content } = req.body
+  const { id } = req.params
+
+  try {
+    const data = await Post.update(
+      { title: title, content: content },
+      { returning: true, where: { id: id } }
+    )
+
+    if (data) {
+      res.json(data)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+exports.deletePost = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const data = await Post.destroy({
+      returning: true,
+      where: {
+        id: id,
+      },
+    })
 
     if (data) {
       res.json(data)
